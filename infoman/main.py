7047d5dbad45c1c964dbb5ec9b3ac84fac7c21ae
yapanits111 <124797894+yapanits111@users.noreply.github.com>
2024-07-09 16:54:48 +0800

    Add files via upload

--- infoman/main.py
+++ infoman/main.py
@@ -1,11 +1,14 @@
 # main
+import os
 import streamlit as st
 from create_application import new_application
 from admin_login import admin_panel
 
 st.set_page_config(page_title="Subway Application Management", page_icon=":sandwich:", layout="wide")
 
-st.image("subway.png", width=200)
+image_path = os.path.abspath("subway.png")
+st.image(image_path, width=200)
+
 st.sidebar.title("Navigation")
 option = st.sidebar.radio("Select a page", ["Create New Application", "Admin Panel"])
 
